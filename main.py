05dc4db8d22425c71567e25e17e24256227b632f
Dikshanta Chapagain <3108dikshanta@gmail.com>
2024-05-16 21:42:54 +0545

    Validators library added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import qrcode as qr
 import customization
 from url_handling import url
 
+
+
 fill_color = input("Enter fill color first you need for qr : ") 
 background_color = input("Enter background color : ") 
 
--- url_handling.py
+++ url_handling.py
@@ -1,8 +1,7 @@
+import validators
+
 def is_valid_url(url):
-    if url.startswith(("http://", "https://", "ftp://")):
-        return True
-    else:
-        return False
+    return validators.url(url)
 
 url = input("Enter the URL you would like to make it for making QR code: ")
 if is_valid_url(url):
